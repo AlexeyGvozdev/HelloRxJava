5c0b80b6e0581e691bb2e35979c4a7e84447762b
Alexey Gvozdev <lesha_chess@mail.ru>
2018-02-06 16:49:43 +0300

    add first lesson

--- app/src/main/java/com/example/alexey/hellorxjava/FirstLesson.kt
+++ app/src/main/java/com/example/alexey/hellorxjava/FirstLesson.kt
@@ -2,11 +2,42 @@ package com.example.alexey.hellorxjava
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 
 class FirstLesson : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_lesson)
+
+        val observable: Observable<String> = Observable.fromArray("one", "two", "three")
+
+        val observer = object : Observer<String> {
+
+            override fun onSubscribe(d: Disposable?) {
+
+            }
+
+            override fun onNext(s: String) {
+                log("onNext: " + s)
+            }
+
+            override fun onError(e: Throwable) {
+                log("onError: " + e)
+            }
+
+            override fun onComplete() {
+                log("onComplete")
+            }
+        }
+
+        observable.subscribe(observer)
+    }
+
+    private fun log(str: String) {
+        Log.d("My Tag", str)
     }
 }
